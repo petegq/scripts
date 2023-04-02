d567e34532eb149f952126eb393069ee46631842
Peter Garside-Quin <petegq@gmail.com>
2023-04-02 19:08:30 +0100

    Get all in array

--- getAll.sh
+++ getAll.sh
@@ -2,4 +2,23 @@
 
 # Get multiple files from github with wget
 
-wget https://raw.githubusercontent.com/petegq/scripts/master/test1.txt
+# wget https://raw.githubusercontent.com/petegq/scripts/master/test1.txt
+
+baseURL=raw.githubusercontent.com/petegq/scripts/master/
+
+fileList=(
+    test1.txt
+    test2.txt
+    test3.txt
+    test4.txt
+    test5.txt
+)
+
+for i in "${fileList[@]}"; do
+    wget https://${baseURL}$i
+done
+
+# for i in {1..5}
+# do
+#     wget https://${baseURL}test$i.txt
+# done
