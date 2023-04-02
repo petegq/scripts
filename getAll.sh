79b7201042d581c8b9edfadbd5c9745ff9e2622c
Peter Garside-Quin <petegq@gmail.com>
2023-04-02 18:33:29 +0100

    Get one

--- getAll.sh
+++ getAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Get a list of files from github with wget
+# Get multiple files from github with wget
 
-wget -q -O - https://api.github.com/repos/petegq/scripts/ | grep -o '"name": "[^"]*' | cut -d '"' -f 4 | grep -v "README.md" | grep -v "LICENSE" | grep -v "getAll.sh" | while read line; do wget -q -O - https://raw.githubusercontent.com/petegq/scripts/master/$line; done
+wget https://raw.githubusercontent.com/petegq/scripts/master/test1.txt
