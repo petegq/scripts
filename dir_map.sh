7c3bdd2333469adbc697da481d646ce4caaeb5e5
Peter Garside-Quin <petegq@gmail.com>
2023-04-11 07:48:17 +0100

    Mute stderr

--- dir_map.sh
+++ dir_map.sh
@@ -10,11 +10,11 @@ then
 fi
 
 DIRECTORY="$1"
-OUTPUT_FILE="map.txt"
+OUTPUT_FILE="test/map.txt"
 
 echo "Generating system map for $DIRECTORY..."
 
-# Using find command to recursively list directories
-find "$DIRECTORY" -type d > "$OUTPUT_FILE"
+# Using find command to recursively list directories and suppress errors
+find "$DIRECTORY" -type d 2>/dev/null > "$OUTPUT_FILE"
 
 echo "System map generated and saved to $OUTPUT_FILE."
